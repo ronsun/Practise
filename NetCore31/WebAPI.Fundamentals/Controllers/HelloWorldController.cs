8212870e682f306df587e7a890a7b298d19be777
ronsun <ron.sun@hotmail.com.tw>
2020-03-15 00:18:38 +0800

    First API, hello world + `services.AddControllers()` and `endpoints.MapControllers()` in Startup.cs is required + "Microsoft.VisualStudio.Web.CodeGeneration.Design" added automatically when creating API Controller.

---
+++ NetCore31/WebAPI.Fundamentals/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Fundamentals.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World WebAPI.");
+        }
+    }
+}
